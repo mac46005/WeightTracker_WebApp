8c6befc466b6f0e6588ac33813b0c71fcda2f0d1
Marco Preciado <mac46005@gmail.com>
2022-08-01 03:09:05 -0500

    Testing out read function

--- src/db_models/PDO_SqliteAccess.php
+++ src/db_models/PDO_SqliteAccess.php
@@ -3,8 +3,9 @@ declare(strict_types = 1);
 
 use WghtTrackApp_ClassLib\DB_Models\Enums\DBIniFile_Enum;
 use WghtTrackApp_ClassLib\DB_Models\Exceptions\DB_IniConfigException;
+use WghtTrackApp_ClassLib\DB_Models\Interfaces\ICRUD;
 
-class PDO_SqliteAccess{
+abstract class PDO_SqliteAccess implements ICRUD{
     protected PDO $db = NULL;
     public function __construct(
         private string $configFilePath = ''
@@ -12,8 +13,10 @@ class PDO_SqliteAccess{
     {
         
     }
+
+
     public function Connect(){
-        
+
         if($dbIniFile = parse_ini_file($this->configFilePath)){
             if($connectionPath = $dbIniFile[DBIniFile_Enum::OPTIONS[6]]){
                 $this->db = new \PDO("sqlite:" . $connectionPath);
--- src/db_models/WghtTrck_DBAccess.php
+++ src/db_models/WghtTrck_DBAccess.php
@@ -4,10 +4,58 @@ declare(strict_types = 1);
 namespace WghtTrackApp_ClassLib\DB_Models;
 
 use PDO_SqliteAccess;
+use PDOException;
 
 class WghtTrck_DbAccessSqlite extends PDO_SqliteAccess{
+
     public function __construct(string $configFilePath)
     {
+
         parent::__construct($configFilePath);
+    }
+
+    public function readOne($id): mixed|bool
+    {
+        $sql = <<<SQL
+        SELECT id,weight
+        FROM entryItems
+        WHERE id = $id
+        SQL;
+        try{
+            $result = $this->db->query($sql);
+            return $result;
+        }catch(\PDOException $ex){
+            throw $ex;
+        }
+        
+        return FALSE;
+    }
+    public function readAll(): mixed|bool
+    {
+        $sql = <<<SQL
+        SELECT id,weight
+        FROM entryItems
+        SQL;
+        try{
+            $result = $this->db->query($sql);
+            return $result;
+        }catch(\PDOException $ex){
+            throw $ex;
+        }
+        return FALSE;
+    }
+    public function write($obj): bool
+    {
+        $sql = <<<SQL
+        SQL;
+        return FALSE;
+    }
+    public function delete($id): bool
+    {
+        return FALSE;
+    }
+    public function update($object): bool
+    {
+        return FALSE;
     }
 }
